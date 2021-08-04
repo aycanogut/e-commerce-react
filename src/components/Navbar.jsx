645fc4b48b6f2164fcca030cb0018f5af1d2927d
Aycan Öğüt <aycanonfire@gmail.com>
2021-08-04 20:07:55 +0300

    updated / classname

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,34 +7,34 @@ import IconClose from '../assets/icons/close.svg'
 
 const Navbar = () => {
   return (
-    <nav className="header__nav">
-      <span className="header__nav__brand">More.</span>
-      <ul className="header__nav__list">
-        <li className="header__nav__list__item">
+    <nav className="nav">
+      <span className="nav__brand">More.</span>
+      <ul className="nav__list">
+        <li className="nav__list__item">
           <img src={IconHamburger} alt="" />
         </li>
-        <li className="header__nav__list__item">
+        <li className="nav__list__item">
           <img src={IconBasket} alt="" />
         </li>
       </ul>
-      <ul className="header__nav__list--hidden">
-        <li className="header__nav__list--hidden-item">
+      <ul className="nav__list--hidden">
+        <li className="nav__list--hidden-item">
           <Link to="/shop">shop</Link>
         </li>
-        <li className="header__nav__list--hidden-item">
+        <li className="nav__list--hidden-item">
           <a href="#">contact</a>
         </li>
-        <li className="header__nav__list--hidden-item">
+        <li className="nav__list--hidden-item">
           <a href="#">about</a>
         </li>
-        <li className="header__nav__list--hidden-item">
+        <li className="nav__list--hidden-item">
           <a href="#">login</a>
         </li>
-        <li className="header__nav__list--hidden-item">
+        <li className="nav__list--hidden-item">
           <a href="#">sign up</a>
         </li>
-        <li className="header__nav__list--hidden-item">
-          <img className="header__nav__list--hidden-item" src={IconClose} alt="" />
+        <li className="nav__list--hidden-item">
+          <img className="nav__list--hidden-item" src={IconClose} alt="" />
         </li>
       </ul>
     </nav>
