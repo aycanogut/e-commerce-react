7b201583748f5e2942dc6ce43021707be5ec98d7
Aycan Öğüt <aycanonfire@gmail.com>
2021-08-04 17:57:32 +0300

    added / hero image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import ImageHero from "../assets/images/hero.svg"
+
 const Header = () => {
   return (
     <header className="header">
       <Navbar />
+      <img src={ImageHero} alt="" />
       <section className="header__title">
         <h1 className="header__title__h1">less isn't more</h1>
         <a className="header__title__button" href="#">
