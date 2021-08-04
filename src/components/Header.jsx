9f58e86685152939c71d1ee50e66734379ec8ab1
Aycan Öğüt <aycanonfire@gmail.com>
2021-08-04 20:07:50 +0300

    updated / bg and button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Navbar from "./Navbar";
-import ImageHero from "../assets/images/hero.svg"
+import ImageHero from "../assets/images/hero.png"
 
 const Header = () => {
   return (
     <header className="header">
       <Navbar />
-      <img src={ImageHero} alt="" />
+      <img className="header__background" src={ImageHero} alt="" />
       <section className="header__title">
         <h1 className="header__title__h1">less isn't more</h1>
-        <a className="header__title__button" href="#">
-          get one
-        </a>
+        <div className="header__title__button">
+          <a className="header__title__button" href="#">
+            get one
+          </a>
+        </div>
       </section>
     </header>
   );
