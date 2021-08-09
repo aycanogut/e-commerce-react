9437c155c84f9d7efb82e159b56633843c7ee56a
Aycan Öğüt <aycanonfire@gmail.com>
2021-08-09 17:50:37 +0300

    updated / shop HTML structure

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -15,36 +15,34 @@ const Shop = () => {
   console.log(products);
   return (
     <main>
-      <div className="container">
+      <section className="shop">
+        <h3 className="shop__title">shop</h3>
+        <div className="shop__card__container">
         {
-          products.map(product =>(
-            <section className="shop" key={product.id}>
-            <div className="shop__card">
-              <article className="shop__card__item">
-                <h2 className="shop__title">{product.name}</h2>
-                <header>
-                  <img className="shop__card__item__image" src={product.imageUrl} alt={product.name} />
-                  {/* <h3 className="shop__card__item__title">Lorem, ipsum.</h3> */}
-                  <p className="shop__card__item__identifier">
-                    Identifier number
-                  </p>
-                </header>
-                <div className="shop__card__item__bottom">
-                  <img className="shop__card__item__bottom__icon" src={IconBasket} alt="" />
-                  <span className="shop__card__item__bottom__old-price">
-                    $999
-                  </span>
-                  <span className="shop__card__item__bottom__new-price">
-                   {product.price}
-                  </span>
-                </div>
-              </article>
-            </div>
-          </section>
-          ))
-        }
-       
-      </div>
+            products.map(product =>(
+                <article className="shop__card" key={product.id}>
+                  <header>
+                  <h3 className="shop__card__title">{product.name}</h3>
+                    <img className="shop__card__item__image" src={product.imageUrl} alt={product.name} />
+                    {/* <h3 className="shop__card__item__title">Lorem, ipsum.</h3> */}
+                    <p className="shop__card__item__identifier">
+                      Identifier number
+                    </p>
+                  </header>
+                  <div className="shop__card__item__bottom">
+                    <img className="shop__card__item__bottom__icon" src={IconBasket} alt="" />
+                    <span className="shop__card__item__bottom__old-price">
+                      $999
+                    </span>
+                    <span className="shop__card__item__bottom__new-price">
+                    {product.price}
+                    </span>
+                  </div>
+                </article>
+            ))
+          }
+        </div>
+      </section>
     </main>
   );
 };
