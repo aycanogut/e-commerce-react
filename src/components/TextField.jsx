523e1990cb1cd8f8a6d9f805fafad8d31907af35
Aycan Öğüt <aycanonfire@gmail.com>
2021-08-11 16:07:29 +0300

    removed / classes

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -4,10 +4,10 @@ import { ErrorMessage, useField } from 'formik';
 export const TextField = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
-    <div className="mb-2">
+    <div className="">
       <label htmlFor={field.name}>{label}</label>
       <input
-        className={`form-control shadow-none ${meta.touched && meta.error && 'is-invalid'}`}
+        className={` ${meta.touched && meta.error && 'is-invalid'}`}
         {...field} {...props}
         autoComplete="off"
       />
