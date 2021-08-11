ae26949fbf415ec57c5a58c0c56000131c9b672c
Aycan Öğüt <aycanonfire@gmail.com>
2021-08-11 15:58:52 +0300

    added / conditional render

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Header from "./Header";
 
 const Layout = ({ children }) => {
+
+  const history = useHistory();
+  const [isHome, setIsHome] = useState(false);
+
+  useEffect(() => {
+    if (history.location.pathname === '/') {
+      setIsHome(true);
+    }
+  }, [])
+
+
   return (
     <div>
-      <Header />
+      {isHome ? <Header /> : <Navbar />}
       {children}
       <Footer />
     </div>
