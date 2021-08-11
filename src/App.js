b360158e5473a4f105a713ea1969fae31f9ccada
Aycan Öğüt <aycanonfire@gmail.com>
2021-08-11 18:49:42 +0300

    added / payment page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const Footer = lazy(() => import("./components/Footer"));
 const SignUp = lazy(() => import("./components/SignUp"));
 const Login = lazy(() => import("./components/Login"));
 const Shop = lazy(() => import("./components/Shop"));
+const Payment = lazy(() => import("./components/Payment"));
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Suspense fallback={<div>Yükleniyor...</div>}>
           <Route path="/" exact component={Shop} />
+          <Route path="/payment" exact component={Payment} />
           <Route path="/footer" component={Footer} />
           <Route path="/melisa" component={SignUp} />
           <Route path="/nihat" component={Login} />
