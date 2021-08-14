090c6b9e4dfb3dd90da1ca5fdea3b76169aec044
Nihat <senius.33@gmail.com>
2021-08-14 15:56:18 +0300

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import "./App.css";
 import React, { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
+import { Loader } from "./components/Loader";
 const Footer = lazy(() => import("./components/Footer"));
 const SignUp = lazy(() => import("./components/SignUp"));
 const Login = lazy(() => import("./components/Login"));
 const Shop = lazy(() => import("./components/Shop"));
 const Payment = lazy(() => import("./components/Payment"));
 const Favorites = lazy(() => import("./components/Favorites"));
+const Error = lazy(() => import("./components/Error"));
 
 function App() {
   return (
     <div className="App">
-     <Suspense fallback={<div>Yükleniyor...</div>}>
-      <Switch>
+      <Suspense fallback={<Loader />}>
+        <Switch>
           <Route path="/" exact component={Shop} />
           <Route path="/payment" exact component={Payment} />
           <Route path="/favorites" exact component={Favorites} />
           <Route path="/footer" component={Footer} />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={SignUp} />
-      </Switch>
+          <Route path="/error" component={Error} />
+        </Switch>
       </Suspense>
     </div>
   );
