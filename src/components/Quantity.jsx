34c61a8cf2dd9aa13101c493997fe9cd2db5796c
Aycan Öğüt <aycanonfire@gmail.com>
2021-08-15 22:04:20 +0300

    added / quantity prop

--- src/components/Quantity.jsx
+++ src/components/Quantity.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import IconPlus from "../assets/icons/plus.svg";
 import IconMinus from "../assets/icons/minus.svg";
 
-const Quantity = () => {
+const Quantity = ({quantity}) => {
 
     return (
       <div className="quantity">
         <button className="quantity__btn" type="button" name="button">
           <img src={IconPlus} alt="" />
         </button>
-        <input type="text" name="name" value="1"></input>
+        <input type="text" name="name" value={quantity}></input>
         <button className="quantity__btn" type="button" name="button">
           <img src={IconMinus} alt="" />
         </button>
